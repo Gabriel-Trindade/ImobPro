d5eb70ded8e3c04fdf9f5cd4d45d9ee149d390ec
Gabriel Trindade <gabriel18.trindade@hotmail.com>
2025-11-12 17:55:49 -0300

    quick update

--- database/migrations/2025_08_06_212147_modify_users_table.php
+++ database/migrations/2025_08_06_212147_modify_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'agent'])->default('agent')->after('password');
+            $table->enum('role', ['admin', 'agent', 'super'])->default('agent')->after('password');
             $table->foreignId('company_id')->nullable()->constrained()->onDelete('set null')->after('role');
         });
 
